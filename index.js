62266570f8f7266ff4cba95777b6a3ea62b544d5
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-09-25 10:47:44 +0530

    eventListener added to dots

--- index.js
+++ index.js
@@ -5,15 +5,20 @@ const dots = document.querySelectorAll('.dot')
 
 
 let index = 0;
+
+// Adding opacity to first dot on first time
+
 dots[0].style.opacity='1'
 
 // positioning the slides
+
 slides.forEach((slide,index)=>{
   slide.style.left=`${index*100}%`
 });
 
 
 // move slide function
+
 const moveSlide = () =>{
   slides.forEach((slide)=>{
     slide.style.transform=`translateX(-${index*100}%)`;
@@ -28,25 +33,38 @@ const removeDotsOpacity = () =>{
   });
 }
 
+dots.forEach((dot,i)=>{
+  dot.addEventListener("click",(e)=>{
+    index=i;
+    removeDotsOpacity();
+    e.target.style.opacity='1'
+    moveSlide();
+  })
+});
+
 // show the previous slide
+
 prevBtn.addEventListener('click',()=>{
-  removeDotsOpacity();
-  if(index===-1) return index;
+  if(index===0) return index;
   index--;
+  console.log(index)
+  removeDotsOpacity();
   dots[index].style.opacity='1'
   moveSlide();
 });
 
 // show the next slide
+
 nextBtn.addEventListener('click',()=>{
-  removeDotsOpacity();
   if(index===slides.length-1) return index;
   index++;
+  removeDotsOpacity();
   dots[index].style.opacity='1'
   moveSlide();
 });
 
 // auto play slide
+
 const autoPlaySlide = () =>{
   removeDotsOpacity();
   if(index===slides.length-1) index= -1;
